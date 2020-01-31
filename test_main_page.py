83c048db197ffa4aa2bcf99cc7c2d3b133d9f423
Klausbear <zimin.mykolay@gmail.com>
2020-01-31 18:06:13 +0200

    Pages: Login&Register

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,29 @@
 from .pages.main_page import MainPage
 
-#def test_guest_can_go_to_login_page(browser):
-#    link = "http://selenium1py.pythonanywhere.com/"
-#    page = MainPage(browser, link)
-#    page.open()
-#    page.go_to_login_page()
+link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
+
+def test_guest_can_go_to_login_page(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
 
 def test_guest_should_see_login_link(browser):
-  link = "http://selenium1py.pythonanywhere.com/"
-  page = MainPage(browser, link)
-  page.open()
-  page.should_be_login_link()
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_should_see_login_url(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+def test_guest_should_see_login_form(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+def test_guest_should_see_register_form(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_register_form()
